07280fb2cc59a8120d17a338039e921eb64b22cc
Torbjørn Vatn <torbjorn.vatn@bekk.no>
2014-01-26 23:20:21 +0100

    adds lejos to path

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,5 @@
 ### PATH ###
 set default_path /usr/bin /usr/sbin /bin /sbin
 set homebrew /usr/local/bin 
-set -gx PATH $homebrew $default_path
+set lejos /opt/leJOS_NXJ_0.9.1beta-3/bin
+set -gx PATH $lejos $homebrew $default_path
